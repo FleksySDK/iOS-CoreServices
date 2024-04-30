f75911c19a3969e447b78039574797c015c34737
Fleksy CI <developers@thingthing.co>
2024-04-30 10:22:05 +0000

    Update package to latest version

--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "iOSCoreServices",
-            url: "https://spm.fleksy.com/iOSCoreServices/v1.15.2/iOSCoreServices-1.15.2.xcframework.zip",
-            checksum: "d046efaa81e62f53da9461cf33cdcee68c251c49a8a61ef413f56b100598a570"
+            url: "https://spm.fleksy.com/iOSCoreServices/v1.15.3/iOSCoreServices-1.15.3.xcframework.zip",
+            checksum: "9b5c9dae4a470e058404abb6ed599a36b012334d7d7a42491ee165af3688f091"
         )
     ]
 )
